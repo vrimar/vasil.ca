524f15095fc25a85c561a5b5589db19c6c29273c
Vasil Rimar <5660247+vrimar@users.noreply.github.com>
2019-08-15 02:27:55 -0400

    Update page title

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import Header from './Header';
 function Layout(props) {
   return <>
     <Head>
-      <title>Vasil Rimar</title>
+      <title>Vasil Rimar -  Software Developer from Toronto</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet"></link>
     </Head>

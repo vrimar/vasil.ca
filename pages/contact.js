c09bb8ed1661df66c53d5d635ae2c77c3e8b24d0
Vasil Rimar <5660247+vrimar@users.noreply.github.com>
2019-08-21 16:54:15 -0400

    Update contact content

--- pages/contact.js
+++ pages/contact.js
@@ -8,7 +8,7 @@ function Contact() {
       <div className="contact-col">
         <h2>Contact<span className="accent-color">.</span></h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor, neque ut efficitur sodales, turpis neque vulputate ex, ac feugiat metus mod sapien rutrum sit amet. In maximus turpis quam, eu luctus lorem lacinia nec.
+          Interested in working with me or have a project in mind? Just drop me a message at <span className="accent-color">info@vasil.ca</span> or use the contact form on the right. I'm currently AVAILABLE for any design, full-stack or front-end gigs.
         </p>
         <SocialIcons />
       </div>

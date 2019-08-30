c56298909750ceaf449004c634e1a1b6e4c3bff6
Vasil Rimar <5660247+vrimar@users.noreply.github.com>
2019-08-30 01:11:02 -0400

    Add google analytics

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,10 @@
 import '../styles/style.scss';
 import Head from 'next/head';
 import Header from './Header';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-146807687-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function Layout(props) {
   return <>

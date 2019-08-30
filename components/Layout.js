5a62e14511ff3c78ac6eb273c1c916077f2cf677
Vasil Rimar <5660247+vrimar@users.noreply.github.com>
2019-08-30 01:18:08 -0400

    Load analytics on layout mount

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,30 @@
 import '../styles/style.scss';
+import React from 'react';
 import Head from 'next/head';
 import Header from './Header';
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('UA-146807687-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
-function Layout(props) {
-  return <>
-    <Head>
-      <title>Vasil Rimar -  Software Developer from Toronto</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet"></link>
-    </Head>
-    <div className=" header-container container">
-      <Header />
-    </div>
-    <div className="bg"></div>
-    {props.children}
-  </>;
+class Layout extends React.Component {
+  componentDidMount() {
+    ReactGA.initialize('UA-146807687-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
+  render() {
+    return <>
+      <Head>
+        <title>Vasil Rimar -  Software Developer from Toronto</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet"></link>
+      </Head>
+      <div className=" header-container container">
+        <Header />
+      </div>
+      <div className="bg"></div>
+      {this.props.children}
+    </>;
+  }
 }
 
 export default Layout;

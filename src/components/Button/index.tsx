b7dc42b7e58324fce81ad3fb5d204b0876dad0d6
Vasil Rimar <5660247+vrimar@users.noreply.github.com>
2024-05-31 04:23:08 -0400

    Add isDisabled prop to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,16 +3,23 @@ import classnames from "classnames";
 
 export interface ButtonProps extends React.ComponentProps<"button"> {
   isLoading?: boolean;
+  isDisabled?: boolean;
   children: React.ReactNode;
 }
 
 export const Button = ({
   isLoading,
+  isDisabled,
   className,
   children,
   ...props
 }: ButtonProps) => {
-  const classes = classnames("ui-button", isLoading && "is-loading", className);
+  const classes = classnames(
+    "ui-button",
+    isLoading && "is-loading",
+    isDisabled && "is-disabled",
+    className,
+  );
 
   const content = (
     <>
